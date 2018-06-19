dfe494cfa243b96814fd568112845a5489c142bd
vinypaulino <vinydos3@gmail.com>
2018-06-18 23:02:52 -0300

    Initial commit

---
+++ app/src/main/java/br/com/anestech/layoutviajem/ListaPacotesActivity.kt
@@ -0,0 +1,12 @@
+package br.com.anestech.layoutviajem
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaPacotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_pacotes)
+    }
+}
